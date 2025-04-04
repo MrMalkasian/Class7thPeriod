086ed570c83563765ea34ca2464100d1f7bb09db
MrMalkasian <garymalkasian@nsd.org>
2025-04-04 14:41:36 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Class7thPeriod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
